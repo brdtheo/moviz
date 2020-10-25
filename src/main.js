f824b8ce92e763fc89466225a8aea19c9944e947
Theo <toto.billardey@gmail.com>
2020-10-25 18:25:47 +0100

    style.css 404 fix

--- src/main.js
+++ src/main.js
@@ -5,20 +5,6 @@ import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
-import Vuetify from 'vuetify/lib'
-new Vuetify({
-  theme: {
-    themes: {
-      light: {
-        primary: '#212121',
-        secondary: '#b0bec5',
-        accent: '#8c9eff',
-        error: '#b71c1c',
-      },
-    },
-  },
-})
-
 new Vue({
   router,
   vuetify,
