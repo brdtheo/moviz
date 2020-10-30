906b99adcc9918e9764877fc3389e519ca0d8434
Theo <toto.billardey@gmail.com>
2020-10-30 22:59:00 +0100

    movie detail init

---
+++ src/views/moviedetail.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <div>{{ $route.params.movieId }}</div>
+</template>
+
+<script>
+//import router from "../router/index";
+//import { db } from "../firebase";
+
+export default {
+  data: () => {
+    return {
+      movie: {},
+    };
+  },
+
+  firestore: {
+    //movie: db.collection("movies").where("id", "==", router.route.params.movieId),
+  },
+
+  mounted() {
+    console.log(this.$route);
+  },
+};
+</script>
