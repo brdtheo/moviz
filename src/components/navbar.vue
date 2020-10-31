374e90a3c9ef9b2007e700b6492e424e1e809bd0
Theo <toto.billardey@gmail.com>
2020-10-31 23:35:00 +0100

    add search feature

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="grey darken-4 white--text">
     <v-app-bar app class="grey darken-4" flat outlined elevate-on-scroll>
-      <v-container class="pa-0">
+      <v-container>
         <v-row>
           <v-col cols="12" class="pa-0 d-flex justify-space-between">
             <v-btn dark icon class="no-active" to="/">
@@ -13,6 +13,7 @@
                 <v-text-field
                   ref="input"
                   @keyup.esc="toggleSearchMode"
+                  @input="querySearch(searchField)"
                   class="search-bar"
                   color="indigo"
                   dark
@@ -24,10 +25,22 @@
                   v-if="searchMode"
                 ></v-text-field>
               </v-fade-transition>
-              <v-btn class="no-active" @click="toggleSearchMode" dark icon v-if="!searchMode">
+              <v-btn
+                class="no-active"
+                @click="toggleSearchMode"
+                dark
+                icon
+                v-if="!searchMode"
+              >
                 <v-icon>mdi-magnify</v-icon>
               </v-btn>
-              <v-btn class="no-active" @click="toggleSearchMode" dark icon v-if="searchMode">
+              <v-btn
+                class="no-active"
+                @click="toggleSearchMode"
+                dark
+                icon
+                v-if="searchMode"
+              >
                 <v-icon>mdi-close</v-icon>
               </v-btn>
             </div>
@@ -40,7 +53,7 @@
         <h2 class="font-weight-light">Your search</h2>
       </div>
       <v-row>
-        <v-col v-if="!queryResult">No results.</v-col>
+        <v-col v-if="queryResult.length == 0">No results.</v-col>
         <v-col
           sm="4"
           md="4"
@@ -51,17 +64,19 @@
           :key="movie.id"
           v-else
         >
-          <v-card>
+          <moviecard>
             <v-img height="100" :src="movie.image"></v-img>
-            <v-card-title>{{ movie.name }}</v-card-title>
-            <v-card-subtitle>{{ movie.year }}</v-card-subtitle>
-            <v-card-text>
+            <span slot="name">{{ movie.name }}</span>
+            <span slot="year">{{ movie.year }}</span>
+            <span slot="description">
               {{ movie.description.substring(0, 97) + "..." }}
-            </v-card-text>
-            <v-card-actions>
-              <v-btn small color="indigo" class="white--text"> infos </v-btn>
-            </v-card-actions>
-          </v-card>
+            </span>
+            <div slot="actions">
+              <v-btn @click="navigate(movie.id)" color="indigo" dark small>
+                infos
+              </v-btn>
+            </div>
+          </moviecard>
         </v-col>
       </v-row>
     </v-container>
@@ -69,20 +84,30 @@
 </template>
 
 <script>
+import router from "../router/index";
 import { db } from "../firebase";
+import moviecard from "./moviecard";
 
 export default {
   name: "navbar",
+
+  components: {
+    moviecard,
+  },
 
   data: () => {
     return {
       searchMode: false,
       searchField: "",
-      queryResult: {},
+      queryResult: [],
     };
   },
 
   methods: {
+    navigate: (movieId) => {
+      router.push({ name: "movie-detail", params: { movieId: movieId } });
+    },
+
     toggleSearchMode() {
       if (this.searchMode) {
         this.searchField = "";
@@ -91,10 +116,23 @@ export default {
         this.searchMode = true;
       }
     },
-  },
 
-  firestore: {
-    queryResult: db.collection("movies").where("name", "==", "Avatar"),
+    querySearch(input) {
+      let vm = this.$data;
+      vm.queryResult = [];
+
+      db.collection("movies")
+        .where("name", "==", input)
+        .get()
+        .then(function (querySnapshot) {
+          querySnapshot.forEach(function (doc) {
+            vm.queryResult.push(doc.data());
+          });
+        })
+        .catch(function (error) {
+          console.log("Error getting documents: ", error);
+        });
+    },
   },
 };
 </script>
@@ -102,5 +140,9 @@ export default {
 <style scoped>
 .search-bar {
   width: 250px;
+}
+
+.v-btn--active.no-active::before {
+  opacity: 0 !important;
 }
 </style>
