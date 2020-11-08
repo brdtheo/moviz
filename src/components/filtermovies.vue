1ec52ec7ca18926b8b557bcebe1d0ecec6d5472b
Theo <toto.billardey@gmail.com>
2020-11-08 01:01:10 +0100

    filter movies

---
+++ src/components/filtermovies.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="mb-5">
+    <v-row justify="space-between">
+      <v-col class="d-flex align-center justify-space-between">
+        <v-btn
+          small
+          color="#e50914"
+          @click="
+            openNetflixOnly();
+            $emit('enable-netflix-only');
+          "
+        >
+          Netflix
+        </v-btn>
+
+        <v-menu left>
+          <template v-slot:activator="{ on }">
+            <v-btn small dark color="indigo" v-on="on">
+              {{ $t("sort") }}
+              <v-icon right dark size="15">mdi-chevron-down</v-icon>
+            </v-btn>
+          </template>
+          <v-list dense>
+            <v-list-item link @click="$emit('sort', 'ascendingName')">
+              <v-list-item-title> {{ $t("name") }}: a > z </v-list-item-title>
+            </v-list-item>
+            <v-list-item link @click="$emit('sort', 'descendingName')">
+              <v-list-item-title>{{ $t("name") }}: z > a</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-col>
+    </v-row>
+    <v-row v-if="netflixOnly">
+      <v-col>
+        <v-chip
+          small
+          close
+          color="#e50914"
+          v-if="netflixOnly"
+          @click:close="
+            closeNetflixOnly();
+            $emit('disable-netflix-only');
+          "
+        >
+          Netflix
+        </v-chip>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "filtermovies",
+
+  data() {
+    return {
+      netflixOnly: false,
+    };
+  },
+
+  methods: {
+    openNetflixOnly() {
+      this.netflixOnly = true;
+    },
+    closeNetflixOnly() {
+      this.netflixOnly = false;
+    },
+  },
+};
+</script>
--- src/views/allmovies.vue
+++ src/views/allmovies.vue
@@ -1,59 +1,105 @@
 <template>
   <div class="py-8 py-sm-16">
-    <v-container class="pa-0">
-      <div class="mb-5 d-flex justify-space-between align-center">
-        <h2 class="font-weight-light">
-          {{ $t("allmovies") + ` (${movies.length})` }}
-        </h2>
-      </div>
-      <v-row>
-        <v-col
-          sm="4"
-          md="3"
-          class="col-12"
-          v-for="movie in movies"
-          :key="movie.id"
-        >
-          <moviecard
-            :image="movie.image"
-            :name="movie.name"
-            :year="movie.year"
-            :description="movie.description.en"
-            :movieId="movie.id"
-            v-if="movie.description && $i18n.locale == 'en'"
-          />
-          <moviecard
-            :image="movie.image"
-            :name="movie.name"
-            :year="movie.year"
-            :description="movie.description.fr"
-            :movieId="movie.id"
-            v-if="movie.description && $i18n.locale == 'fr'"
-          />
-        </v-col>
-      </v-row>
-    </v-container>
+    <filtermovies
+      @enable-netflix-only="enableNetflixOnly()"
+      @disable-netflix-only="disableNetflixOnly()"
+      @sort="sort($event)"
+    />
+
+    <div class="mb-5 d-flex justify-space-between align-center">
+      <h2 class="font-weight-light">
+        {{ $t("allmovies") + ` (${movies.length})` }}
+      </h2>
+    </div>
+    <v-row>
+      <v-col
+        sm="4"
+        md="3"
+        class="col-12"
+        v-for="movie in movies"
+        :key="movie.id"
+      >
+        <moviecard
+          :image="movie.image"
+          :name="movie.name"
+          :year="movie.year"
+          :description="movie.description.en"
+          :movieId="movie.id"
+          v-if="movie.description && $i18n.locale == 'en'"
+        />
+        <moviecard
+          :image="movie.image"
+          :name="movie.name"
+          :year="movie.year"
+          :description="movie.description.fr"
+          :movieId="movie.id"
+          v-if="movie.description && $i18n.locale == 'fr'"
+        />
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
+import filtermovies from "../components/filtermovies";
 import moviecard from "../components/moviecard";
 import { db } from "../firebase";
 
 export default {
   components: {
     moviecard,
+    filtermovies,
   },
 
   data() {
     return {
       movies: {},
+
+      // filters
+      displayNetflixOnly: false,
     };
   },
 
+  methods: {
+    enableNetflixOnly() {
+      if (!this.displayNetflixOnly) {
+        this.displayNetflixOnly = true;
+        this.$bind(
+          "movies",
+          db.collection("movies").where("links.netflix", "!=", "")
+        );
+      }
+    },
 
-  firestore: {
-    movies: db.collection("movies").orderBy("name"),
+    disableNetflixOnly() {
+      this.displayNetflixOnly = false;
+      this.$bind("movies", db.collection("movies").orderBy("name"));
+    },
+
+    sort(options) {
+      if (this.displayNetflixOnly) {
+        if (options == "ascendingName") {
+          console.log("netflix + ascending name");
+        }
+        if (options == "descendingName") {
+          console.log("netflix + descending name");
+        }
+      }
+      if (!this.displayNetflixOnly) {
+        if (options == "ascendingName") {
+          console.log("no netflix + ascending name");
+          this.$bind("movies", db.collection("movies").orderBy("name", "asc"));
+        }
+        if (options == "descendingName") {
+          console.log("no netflix + descending name");
+          this.$bind("movies", db.collection("movies").orderBy("name", "desc"));
+        }
+      }
+    },
+  },
+
+  created() {
+    this.$bind("movies", db.collection("movies"));
   },
 };
 </script>
