ae2dc04763f9b5ec3b4ac2b565f3f83f7babb61f
Theo <toto.billardey@gmail.com>
2020-11-25 20:00:05 +0100

    remove footer social links

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,24 +1,22 @@
 <template>
   <v-footer class="grey darken-4 white--text" padless>
     <v-container>
-      <v-row justify="center" align="center">
-        <v-col cols="6">
+      <v-row justify="space-between" align="center">
+        <v-col cols="7">
           <v-btn
-            icon
-            dark
-            class="mr-sm-2 mr-md-4"
-            v-for="link in social"
-            :key="link.name"
-            :href="link.url"
-            :target="link.target"
+            small
+            href="https://github.com/brdtheo/moviz/"
+            target="_blank"
             rel="noreferrer"
           >
-            <v-icon dense>{{ "mdi-" + link.icon }}</v-icon>
+            <v-icon dense class="mr-2">mdi-github</v-icon>
+            <span>View on Github</span>
           </v-btn>
         </v-col>
-        <v-col cols="6" class="text-right">
+        <v-col cols="5" class="text-right">
           <span>{{ new Date().getFullYear() }}</span>
-          <span class="mx-5">•</span> <strong>Moviz</strong>
+          <span class="mx-2">•</span>
+          <strong>Moviz</strong>
         </v-col>
       </v-row>
     </v-container>
@@ -28,36 +26,5 @@
 <script>
 export default {
   name: "Footer",
-
-  data: () => {
-    return {
-      social: [
-        {
-          name: "codepen",
-          icon: "codepen",
-          url: "https://codepen.io/theo-billardey",
-          target: "_blank",
-        },
-        {
-          name: "linkedin",
-          icon: "linkedin",
-          url: "https://www.linkedin.com/in/theo-billardey/",
-          target: "_blank",
-        },
-        {
-          name: "facebook",
-          icon: "facebook",
-          url: "https://www.facebook.com/profile.php?id=100017396689455",
-          target: "_blank",
-        },
-        {
-          name: "github",
-          icon: "github",
-          url: "https://github.com/brdtheo",
-          target: "_blank",
-        },
-      ],
-    };
-  },
 };
 </script>
